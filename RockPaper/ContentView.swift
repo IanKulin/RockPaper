ff3d81599b92ba39cfa2a41c5c8c213f1b442735
Ian Bailey <IanKulin@kulin.com.au>
2022-09-08 18:38:27 +0800

    Refactor to stop using int's into an array for the hand shapes by creating new enum for Shape

--- RockPaper/ContentView.swift
+++ RockPaper/ContentView.swift
@@ -7,27 +7,43 @@
 
 import SwiftUI
 
+// Some terminology: a "shape" is the shape made with the hand
+// in each "play" of the game
+
+
+
+
+enum Shape: String {
+    case rock = "🪨"
+    case paper = "📃"
+    case scissors = "✂️"
+    
+    static func random() -> Shape {
+        switch Int.random(in: 0...2) {
+            case 0: return self.rock
+            case 1: return self.paper
+            default: return self.scissors
+        }
+    }
+}
+
+
 
 struct ContentView: View {
     @State var score = 0
     @State var goalIsWinThisTurn = Bool.random()
-    @State var userSelection = 0
-    @State var computerSelection = 0
+    @State var userSelection: Shape = .rock
+    @State var computerSelection: Shape = .rock
     @State var winText = ""
     @State var numberOfPlays = 0
     @State var showScores = false
 
 
     // game has two modes - revealResult - buttons don't work, and we can see the computer result
-    // or not readyToPlay - user can chose their play
+    // or !revealResult - user can chose their play
     @State var revealResult = false
     
-    let rock = 0
-    let paper = 1
-    let scissors = 2
-    let rpsEmoji = ["🪨", "📃", "✂️"]
     
-        
     var body: some View {
         ZStack{
             LinearGradient(colors: [.white, .gray], startPoint: .top, endPoint: .bottom)
@@ -55,23 +71,23 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Button(rpsEmoji[rock]) {
-                        processButton(selection: rock)
+                    Button(Shape.rock.rawValue) {
+                        processButton(selection: .rock)
                     }
                     Spacer()
-                    Button(rpsEmoji[paper]) {
-                        processButton(selection: paper)
+                    Button(Shape.paper.rawValue) {
+                        processButton(selection: .paper)
                     }
                     Spacer()
-                    Button(rpsEmoji[scissors]){
-                        processButton(selection: scissors)
+                    Button(Shape.scissors.rawValue){
+                        processButton(selection: .scissors)
                     }
                     Spacer()
                 }.font(.system(size: 60))
                 
                 Spacer()
                 if revealResult {
-                    Text(rpsEmoji[computerSelection])
+                    Text(computerSelection.rawValue)
                         .font(.system(size: 200))
                     Text(winText).font(.title)
                     Spacer()
@@ -87,7 +103,7 @@ struct ContentView: View {
                     .buttonStyle(CustomButtonStyle())
                 }
                 else {
-                    Text(rpsEmoji[computerSelection])
+                    Text(computerSelection.rawValue)
                         .font(.system(size: 200))
                         .hidden()
                     Text(winText)
@@ -104,55 +120,49 @@ struct ContentView: View {
         }
     }
     
-    func didUserWin(user: Int, computer: Int) -> Bool {
-        switch(user) {
-        case rock:
-            switch(computer) {
-            case scissors: return true
+    func didUserWin(user: Shape, computer: Shape) -> Bool {
+        switch user {
+        case .rock:
+            switch computer {
+            case .scissors: return true
             default: return false
             }
-        case paper:
-            switch(computer) {
-            case rock: return true
+        case .paper:
+            switch computer {
+            case .rock: return true
             default: return false
             }
-        case scissors:
-            switch(computer) {
-            case paper: return true
+        case .scissors:
+            switch computer {
+            case .paper: return true
             default: return false
             }
-        default:
-            assert(false)
-            return false
         }
     }
     
-    func didComputerWin(user: Int, computer: Int) -> Bool {
-        switch(computer) {
-        case rock:
-            switch(user) {
-            case scissors: return true
+    func didComputerWin(user: Shape, computer: Shape) -> Bool {
+        switch computer {
+        case .rock:
+            switch user  {
+            case .scissors: return true
             default: return false
             }
-        case paper:
-            switch(user) {
-            case rock: return true
+        case .paper:
+            switch user  {
+            case .rock: return true
             default: return false
             }
-        case scissors:
-            switch(user) {
-            case paper: return true
+        case .scissors:
+            switch user  {
+            case .paper: return true
             default: return false
             }
-        default:
-            assert(false)
-            return false
         }
     }
     
-    func processButton(selection: Int) {
+    func processButton(selection: Shape) {
         if !revealResult {
-            computerSelection = Int.random(in: 0...2)
+            computerSelection = Shape.random()
             userSelection = selection
             numberOfPlays += 1
             showScores = numberOfPlays == 10
